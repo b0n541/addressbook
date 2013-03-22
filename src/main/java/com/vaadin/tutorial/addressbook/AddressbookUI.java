8b86aa89a80da60b000ea09582dc4b2d1c033f1b
b0n541 <b0n541@github>
2013-03-22 06:27:42 +0100

    deleted obsolet GUI components

--- src/main/java/com/vaadin/tutorial/addressbook/AddressbookUI.java
+++ src/main/java/com/vaadin/tutorial/addressbook/AddressbookUI.java
@@ -2,18 +2,13 @@ package com.vaadin.tutorial.addressbook;
 
 import com.google.gwt.thirdparty.guava.common.eventbus.EventBus;
 import com.vaadin.annotations.Title;
-import com.vaadin.data.fieldgroup.FieldGroup;
 import com.vaadin.data.util.IndexedContainer;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.tutorial.addressbook.event.InitContactListEvent;
 import com.vaadin.tutorial.addressbook.model.AddressbookModel;
 import com.vaadin.tutorial.addressbook.view.ContactEditorImpl;
 import com.vaadin.tutorial.addressbook.view.ContactTreeImpl;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.FormLayout;
 import com.vaadin.ui.HorizontalSplitPanel;
-import com.vaadin.ui.Table;
-import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 
 /* 
@@ -25,22 +20,9 @@ import com.vaadin.ui.UI;
 @Title("Addressbook")
 public class AddressbookUI extends UI {
 
+	private static final long serialVersionUID = 1L;
+
 	EventBus eventBus = new EventBus();
-
-	/* User interface components are stored in session. */
-	private Table contactList = new Table();
-	private TextField searchField = new TextField();
-	private Button addNewContactButton = new Button("New");
-	private Button removeContactButton = new Button("Remove this contact");
-	private FormLayout editorLayout = new FormLayout();
-	private FieldGroup editorFields = new FieldGroup();
-
-	private static final String FNAME = "First Name";
-	private static final String LNAME = "Last Name";
-	private static final String COMPANY = "Company";
-	private static final String[] fieldNames = new String[] { FNAME, LNAME,
-			COMPANY, "Mobile Phone", "Work Phone", "Home Phone", "Work Email",
-			"Home Email", "Street", "City", "Zip", "State", "Country" };
 
 	/*
 	 * Any component can be bound to an external data source. This example uses
